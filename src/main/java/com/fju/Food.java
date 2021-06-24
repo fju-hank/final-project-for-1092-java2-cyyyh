835bbc272582d0f7bab8747c4babdccd89577eb4
yenhsi <cynthiahsieh218@gmail.com>
2021-06-24 17:50:37 +0800

    add class food

---
+++ src/main/java/com/fju/Food.java
@@ -0,0 +1,16 @@
+package com.fju;
+
+public class Food {
+    String id;
+    String name;
+    int price;
+
+    public Food(String id, String name, int price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public void print(){
+        System.out.println(id + "\t" + name + "\t" + price);
+    }
+}
