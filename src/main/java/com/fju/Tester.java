aa56685eb944384456e006589872b01f81e735b6
yenhsi <cynthiahsieh218@gmail.com>
2021-06-26 16:17:28 +0800

    test

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -32,6 +32,9 @@ public class Tester {
                 }
             }
             c1.start();
+            /*
+             * sleep   是為了等時間到再印出點餐明細
+             */
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -48,61 +51,6 @@ public class Tester {
                 bln = true;
             }
         }while (bln);
-        /*
-        System.out.println("Welcome!" + "\n" + "Menu:");
-        Food food1 = new Food("a", "cake", 75);
-        Food food2 = new Food("b", "bread", 55);
-        Food food3 = new Food("c", "cookies", 45);
-        Drink drink1 = new Drink("d", "coffee", 40, true);
-        Drink drink2 = new Drink("e", "coffee", 40, false);
 
-        food1.print();
-        food2.print();
-        food3.print();
-        drink1.print();
-        drink2.print();
-
-        Scanner scanner = new Scanner(System.in);
-        int price = 0;
-        int total;
-        int a = 75;
-        int b = 55;
-        int c = 45;
-        int d = 40;
-        int e = 40;
-
-        while(true){
-            System.out.println("Please order, if finished, please enter 'f' , Price: " + price);
-            String s = scanner.next();
-            if (s.equals("a")){
-                price = price + a;
-            }else if(s.equals("b")){
-                price = price + b;
-            }else if(s.equals("c")){
-                price = price + c;
-            }else if(s.equals("d")){
-                price = price + d;
-            }else if(s.equals("e")){
-                price = price + e;
-            }else if(s.equals("f")){
-                total = price;
-                System.out.println("________________________________________________________");
-                System.out.println("Total: " + total + ", thank you!!!");
-                if (total >= 200){
-                    System.out.println("You can join a lucky draw!!!!!");
-                    System.out.println("________________________________________________________");
-                    Coupon c1 = new Coupon();
-                    Coupon c2 = new Coupon();
-                    c1.setName("Food Coupon");
-                    c2.setName("Drink Coupon");
-                    c1.start();
-                    c2.start();
-                } else {
-                    break;
-                }
-                break;
-            }
-        }
-*/
     }
 }
